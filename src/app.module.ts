d1656a82ef318424cf025472d543ddc4a2316dc7
VictorAgahi <victor@agahi.com>
2026-03-01 22:48:14 +0100

    grpc connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config/app-config.module.js';
 import { DatabaseModule } from './providers/database/database.module.js';
 import { PostModule } from './modules/post/post.module.js';
+import { GrpcClientModule } from './grpc/grpc-client.module.js';
 
 @Module({
-  imports: [AppConfigModule, DatabaseModule, PostModule],
+  imports: [AppConfigModule, DatabaseModule, PostModule, GrpcClientModule],
 })
 export class AppModule {}
--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PostController } from './post.controller.js';
+import { PostGrpcController } from './post.grpc.controller.js';
 
 @Module({
-  controllers: [PostController],
+  controllers: [PostController, PostGrpcController],
 })
 export class PostModule {}
