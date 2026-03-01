b0f90c514ce8350bee04485458095b6b5d0ee973
VictorAgahi <victor@agahi.com>
2026-03-01 18:14:05 +0100

    fix esm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppConfigModule } from './config/app-config.module';
-import { DatabaseModule } from './providers/database/database.module';
-import { PostModule } from './modules/post/post.module';
+import { AppConfigModule } from './config/app-config.module.js';
+import { DatabaseModule } from './providers/database/database.module.js';
+import { PostModule } from './modules/post/post.module.js';
 
 @Module({
   imports: [AppConfigModule, DatabaseModule, PostModule],
--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { PostController } from './post.controller';
+import { PostController } from './post.controller.js';
 
 @Module({
   controllers: [PostController],
