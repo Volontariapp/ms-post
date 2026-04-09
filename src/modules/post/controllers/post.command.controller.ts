dcfb26e289219eb75b63413df2e69292c63f76d2
VictorAgahi <victor@agahi.com>
2026-04-09 11:30:34 +0200

    chore(config): update config library and standardize architecture

--- src/modules/post/controllers/post.command.controller.ts
+++ src/modules/post/controllers/post.command.controller.ts
@@ -14,17 +14,17 @@ import {
 @Controller()
 export class PostCommandController {
   @GrpcMethod(POST_SERVICE_NAME, POST_GRPC_METHODS.CREATE_POST)
-  async createPost(_command: CreatePostCommand): Promise<CreatePostResponse> {
+  createPost(_command: CreatePostCommand): Promise<CreatePostResponse> {
     throw new Error('Method not implemented.');
   }
 
   @GrpcMethod(POST_SERVICE_NAME, POST_GRPC_METHODS.UPDATE_POST)
-  async updatePost(_command: UpdatePostCommand): Promise<UpdatePostResponse> {
+  updatePost(_command: UpdatePostCommand): Promise<UpdatePostResponse> {
     throw new Error('Method not implemented.');
   }
 
   @GrpcMethod(POST_SERVICE_NAME, POST_GRPC_METHODS.DELETE_POST)
-  async deletePost(_command: DeletePostCommand): Promise<DeletePostResponse> {
+  deletePost(_command: DeletePostCommand): Promise<DeletePostResponse> {
     throw new Error('Method not implemented.');
   }
 }
