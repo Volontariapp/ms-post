585707312655948895df916289d9770a9c29ce90
VictorAgahi <victor@agahi.com>
2026-04-13 20:50:25 +0200

    features: health check + database registering

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,11 @@ import type { CustomConfig } from './config/base-config.js';
 import { DatabaseModule } from './providers/database/database.module.js';
 import { PostModule } from './modules/post/post.module.js';
 import { GrpcClientModule } from './grpc/grpc-client.module.js';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+import { GlobalExceptionFilter } from '@volontariapp/errors-nest';
+import { GrpcValidationPipe } from '@volontariapp/validation-nest';
+import { HealthModule } from '@volontariapp/health-check-nest';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
   imports: [DatabaseModule, PostModule, GrpcClientModule],
@@ -14,9 +19,27 @@ export class AppModule {
       module: AppModule,
       imports: [
         AppConfigModule.forRoot(config),
-        DatabaseModule,
+        DatabaseModule.forRoot(config.db),
+        TerminusModule.forRoot({}),
+        HealthModule.register({
+          databases: ['postgres'],
+          failOnMissingProvider: true,
+        }),
         PostModule,
         GrpcClientModule,
+      ],
+      providers: [
+        {
+          provide: APP_FILTER,
+          useClass: GlobalExceptionFilter,
+        },
+        {
+          provide: APP_PIPE,
+          useFactory: (): GrpcValidationPipe =>
+            new GrpcValidationPipe({
+              enumMaps: {},
+            }),
+        },
       ],
     };
   }
